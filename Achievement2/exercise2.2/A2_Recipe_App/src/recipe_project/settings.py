53718c42021917458963209ff07a403ea2a231f2
jkeefe77 <jvkeefe@gmail.com>
2024-01-12 14:03:42 -0500

    Update settings.py

--- Achievement2/exercise2.2/A2_Recipe_App/src/recipe_project/settings.py
+++ Achievement2/exercise2.2/A2_Recipe_App/src/recipe_project/settings.py
@@ -120,15 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(
-        BASE_DIR,
-        "static",
-    ),
-    os.path.join(BASE_DIR, "recipes/static/recipes"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 django_heroku.settings(locals())
