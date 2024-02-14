9438cebed1f53a23a565888df58304a1c47f28df
jkeefe77 <jvkeefe@gmail.com>
2024-02-14 11:52:56 -0500

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'stark-island-85026.herokuapp.com', 'stark-island-85026-45df70d91760.herokuapp.com']
 
 
 # Application definition
